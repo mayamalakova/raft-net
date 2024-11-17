8a054708a09e25307e1f80a524711cb0d0f289f5
Maya Malakova <maya.malakova@gmail.com>
2024-11-17 21:02:45 +0100

    GRPC server client poc

---
+++ Raft.RpcServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+using Shared;
+
+namespace Raft;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var port = int.Parse(args[0]);
+        var server = new Server
+        {
+            Services = { Svc.BindService(new MyService()) },
+            Ports = { new ServerPort("0.0.0.0", port, ServerCredentials.Insecure) }
+        };
+        server.Start();
+        Console.WriteLine($"Server listening at port {port}. Press any key to terminate");
+        Console.ReadKey();
+    }
+}
