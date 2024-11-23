fbd49b34a8e695561f8d794d8607e5ba329fd6cc
mayamalakova <maya.malakova@gmail.com>
2024-11-23 17:39:56 +0100

    Implement info service

--- Raft.Cli/RaftClient.cs
+++ Raft.Cli/RaftClient.cs
@@ -1,19 +1,30 @@
-﻿using CommandLine;
-using Grpc.Core;
+﻿using Grpc.Core;
 
 namespace Raft.Cli;
 
-
-
-public class RaftClient(string host, int port)
+public class RaftClient
 {
-    
+    private readonly PingSvc.PingSvcClient _pingClient;
+    private readonly NodeInfoSvc.NodeInfoSvcClient _infoClient;
+
+    public RaftClient(string host, int port)
+    {
+        var channel = new Channel(host, port, ChannelCredentials.Insecure);
+        _pingClient = new PingSvc.PingSvcClient(channel);
+        _infoClient = new NodeInfoSvc.NodeInfoSvcClient(channel);
+    }
+
     public void Ping()
     {
-        var channel = new Channel(host, port, ChannelCredentials.Insecure);  
-        var client = new PingSvc.PingSvcClient(channel);
-        var reply = client.Ping(new PingRequest());
-        
+        var reply = _pingClient.Ping(new PingRequest());
+
+        Console.WriteLine(reply);
+    }
+
+    public void Info()
+    {
+        var reply = _infoClient.GetInfo(new NodeInfoRequest());
+
         Console.WriteLine(reply);
     }
 }
