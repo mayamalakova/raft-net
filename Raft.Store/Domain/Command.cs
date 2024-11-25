e2b038155ddf32f587321816b7baac49efb21594
Maya Malakova <maya.malakova@gmail.com>
2024-11-25 15:45:00 +0100

    add state machine

---
+++ Raft.Store/Domain/Command.cs
@@ -0,0 +1,22 @@
+﻿namespace Raft.Store.Domain;
+
+public enum CommandOperation
+{
+    Assignment,
+    Plus,
+    Minus
+}
+
+public class Command
+{
+    public string Variable { get; set; }
+    public CommandOperation Operation { get; set; }
+    public int Literal { get; set; }
+
+    public Command(string variable, CommandOperation operation, int literal)
+    {
+        Variable = variable;
+        Operation = operation;
+        Literal = literal;
+    }
+}
