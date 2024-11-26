b079edfc78f4858a660696b40d79d23e31e144bc
Maya Malakova <maya.malakova@gmail.com>
2024-11-26 09:09:18 +0100

    add leader log replication test

---
+++ Raft.Node.Tests/LogReplicationServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using NSubstitute;
+using NUnit.Framework;
+using Raft.Node.Communication;
+using Raft.Store;
+using Raft.Store.Domain;
+using Shouldly;
+
+namespace Raft.Node.Tests;
+
+public class LogReplicationServiceTests
+{
+    private INodeStateStore _mockStateStore;
+    private LogReplicationService _logReplicationService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _mockStateStore = Substitute.For<INodeStateStore>();
+        _logReplicationService = new LogReplicationService(_mockStateStore);
+    }
+
+    [Test]
+    public void LeaderShouldApplyCommand()
+    {
+        _mockStateStore.Role.Returns(NodeType.Leader);
+        var mockCallContext = Substitute.For<ServerCallContext>();
+        mockCallContext.Host.Returns("localhost");
+        
+        var reply = _logReplicationService.ApplyCommand(
+            new CommandRequest() { Variable = "A", Operation = "=", Literal = 5 }, mockCallContext);
+        reply.Result.ShouldBe(new CommandReply() { Result = "Success at localhost" });
+    }
+
+
+}
