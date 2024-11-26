f5e21852a332ae9cfb637b5b5bdc1ad3e7aa4833
Maya Malakova <maya.malakova@gmail.com>
2024-11-26 10:02:45 +0100

    tidy up test set up

--- Raft.Node.Tests/LogReplicationServiceTests.cs
+++ Raft.Node.Tests/LogReplicationServiceTests.cs
@@ -29,7 +29,7 @@ public class LogReplicationServiceTests
         _mockStateStore.Role.Returns(NodeType.Leader);
         var mockCallContext = Substitute.For<ServerCallContext>();
         mockCallContext.Host.Returns("localhost");
-        
+
         var reply = _logReplicationService.ApplyCommand(
             new CommandRequest() { Variable = "A", Operation = "=", Literal = 5 }, mockCallContext);
         reply.Result.ShouldBe(new CommandReply() { Result = "Success at localhost" });
@@ -38,21 +38,25 @@ public class LogReplicationServiceTests
     [Test]
     public void FollowerShouldForwardToLeader()
     {
-        _mockStateStore.Role.Returns(NodeType.Follower);
         var leaderAddress = new NodeAddress("localhost", 5001);
-        var mockCommandClient = Substitute.For<CommandSvc.CommandSvcClient>();
-        _channelPool.GetCommandServiceClient(leaderAddress).Returns(mockCommandClient);
+        _mockStateStore.Role.Returns(NodeType.Follower);
         _mockStateStore.LeaderAddress.Returns(leaderAddress);
-        var mockCallContext = Substitute.For<ServerCallContext>();
-        mockCallContext.Host.Returns("localhost");
-        var commandRequest = new CommandRequest() { Variable = "A", Operation = "=", Literal = 5 };
-        mockCommandClient.ApplyCommandAsync(commandRequest).Returns(
-            ClientMockHelpers.CreateAsyncUnaryCall(new CommandReply() { Result = "Success at leader" }));
 
-        var reply = _logReplicationService.ApplyCommand(commandRequest, mockCallContext);
-        
+        var commandRequest = new CommandRequest() { Variable = "A", Operation = "=", Literal = 5 };
+        CreateMockCommandClient(leaderAddress, commandRequest,
+            new CommandReply() { Result = "Success at leader" });
+
+        var reply = _logReplicationService.ApplyCommand(commandRequest, Substitute.For<ServerCallContext>());
+
         reply.Result.ShouldBe(new CommandReply() { Result = "Success at leader" });
     }
-    
 
+    private void CreateMockCommandClient(NodeAddress targetAddress, CommandRequest command,
+        CommandReply reply)
+    {
+        var mockCommandClient = Substitute.For<CommandSvc.CommandSvcClient>();
+        mockCommandClient.ApplyCommandAsync(command)
+            .Returns(ClientMockHelpers.CreateAsyncUnaryCall(reply));
+        _channelPool.GetCommandServiceClient(targetAddress).Returns(mockCommandClient);
+    }
 }
