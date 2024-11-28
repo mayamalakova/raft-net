de214ba35961e8febf90194e5b2d24b4987e9a28
Maya Malakova <maya.malakova@gmail.com>
2024-11-28 08:43:39 +0100

    inline comments with follower steps

--- Raft.Node/Communication/Services/AppendEntriesService.cs
+++ Raft.Node/Communication/Services/AppendEntriesService.cs
@@ -10,28 +10,41 @@ public class AppendEntriesService(INodeStateStore stateStore) : AppendEntriesSvc
     public override Task<AppendEntriesReply> AppendEntries(AppendEntriesRequest request, ServerCallContext context)
     {
         Console.WriteLine($"Appending entries {request}");
+        // 1. reply false if term < currentTerm
+        // 2. reply false if log doesn't contain an entry at prevLogIndex whose term matches prevLogTerm
         if (stateStore.CurrentTerm > request.Term || stateStore.GetTermAtIndex(request.PrevLogIndex) != request.PrevLogTerm)
         {
-            return Task.FromResult(new AppendEntriesReply
-            {
-                Term = stateStore.CurrentTerm,
-                Success = false
-            });
+            return Fail();
         }
-
+        // TODO 3. if an existing entry conflicts with a new one (same index, different term) delete it and all that follow
+        // 4. append any new entries not already in the log
         foreach (var entry in request.EntryCommands)
         {
             stateStore.AppendLogEntry(entry.ToCommand(), request.Term);
         }
-
+        // 5. update commitIndex
         if (request.LeaderCommit > stateStore.CommitIndex)
         {
             stateStore.CommitIndex = Math.Min(request.LeaderCommit, stateStore.LogLength - 1);
         }
+        return Success();
+    }
+
+    private Task<AppendEntriesReply> Success()
+    {
         return Task.FromResult(new AppendEntriesReply
         {
             Term = stateStore.CurrentTerm,
             Success = true
+        });
+    }
+
+    private Task<AppendEntriesReply> Fail()
+    {
+        return Task.FromResult(new AppendEntriesReply
+        {
+            Term = stateStore.CurrentTerm,
+            Success = false
         });
     }
 
