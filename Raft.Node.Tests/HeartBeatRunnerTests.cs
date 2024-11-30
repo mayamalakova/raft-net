c208dfac888df1a705c8e0adb8d9d88cf7e326ae
Maya Malakova <maya.malakova@gmail.com>
2024-11-30 15:57:06 +0100

    Add HeartBeatRunner.cs

---
+++ Raft.Node.Tests/HeartBeatRunnerTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using Raft.Node.HeatBeat;
+using Shouldly;
+
+namespace Raft.Node.Tests;
+
+class BeatSink
+{
+    public int BeatsCount { get; set; } = 0;
+
+    public void SendBeat()
+    {
+        BeatsCount++;
+    }
+}
+
+public class HeartBeatRunnerTests
+{
+    private BeatSink _beatSink;
+    private HeartBeatRunner _heartBeatRunner;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _beatSink = new BeatSink();
+        _heartBeatRunner = new HeartBeatRunner(50, _beatSink.SendBeat);
+    }
+
+    [Test]
+    public async Task ShouldBeat()
+    {
+        _heartBeatRunner.StartBeating();
+        await Task.Delay(100);
+        _beatSink.BeatsCount.ShouldBeGreaterThan(0);
+    }
+
+    [Test]
+    public async Task ShouldStopBeating()
+    {
+        _heartBeatRunner.StartBeating();
+        _heartBeatRunner.StopBeating();
+        var oldBeatCount = _beatSink.BeatsCount;
+        await Task.Delay(50);
+        _beatSink.BeatsCount.ShouldBe(oldBeatCount);
+    }
+}
---
+++ Raft.Node/HeatBeat/HeartBeatRunner.cs
@@ -0,0 +1,34 @@
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace Raft.Node.HeatBeat;
+
+public class HeartBeatRunner
+{
+    private readonly Action _action;
+    private readonly Timer _timer;
+
+    public HeartBeatRunner(int interval, Action action)
+    {
+        _action = action;
+        _timer = new Timer(interval); 
+        _timer.AutoReset = true; 
+        _timer.Elapsed += PerformAction;
+    }
+
+    public void StartBeating()
+    {
+        _timer.Enabled = true;
+    }
+
+    public void StopBeating()
+    {
+        _timer.Enabled = false;
+    }
+    
+    private void PerformAction(object? sender, ElapsedEventArgs e)
+    {
+        Console.WriteLine($"Action executed at {DateTime.Now:HH:mm:ss.fff}");
+        _action();
+    }
+}
