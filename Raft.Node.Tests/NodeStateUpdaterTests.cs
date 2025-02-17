24b44cdfcf0d5ae9ba94af20e99f1a44a8282443
Maya Malakova <maya.malakova@gmail.com>
2025-02-17 18:27:46 +0100

    extract ReplicationStateManager.cs

---
+++ Raft.Node.Tests/NodeStateUpdaterTests.cs
@@ -0,0 +1,84 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Raft.Node.Communication.Services.Cluster;
+using Raft.Store;
+using Raft.Store.Domain;
+using Raft.Store.Domain.Replication;
+using Raft.Store.Memory;
+using Shouldly;
+
+namespace Raft.Node.Tests;
+
+public class NodeStateUpdaterTests
+{
+    private INodeStateStore _nodeStateStore;
+    private IClusterNodeStore _clusterNodeStore;
+    private ReplicationStateManager _replicationStateManager;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _nodeStateStore = new NodeStateStore();
+        _clusterNodeStore = Substitute.For<IClusterNodeStore>();
+        _replicationStateManager = new ReplicationStateManager(_nodeStateStore, _clusterNodeStore);
+    }
+
+    private void PopulateLog(int[] terms)
+    {
+        foreach (var term in terms)
+        {
+            var command = new Command("a", CommandOperation.Assignment, 1);
+            _nodeStateStore.AppendLogEntry(new LogEntry(command, term));
+        }
+    }
+
+    [Test]
+    public void ShouldUpdateState_WhenAllMatchingIndexesAgree()
+    {
+        PopulateLog([1, 1]);
+        _nodeStateStore.CurrentTerm = 1;
+        _clusterNodeStore.GetMatchingIndex("node1").Returns(1);
+        _clusterNodeStore.GetMatchingIndex("node2").Returns(1);
+        
+        _replicationStateManager.UpdateCommitIndex([
+            new NodeInfo("node1", new NodeAddress("host1", 1)), 
+            new NodeInfo("node2", new NodeAddress("host2", 2))
+        ]);
+        
+        _nodeStateStore.CommitIndex.ShouldBe(1);
+    }
+    
+    [Test]
+    public void ShouldUpdateState_WhenMajorityMatchingIndexesAgree()
+    {
+        PopulateLog([1, 1, 1]);
+        _nodeStateStore.CurrentTerm = 1;
+        _clusterNodeStore.GetMatchingIndex("node1").Returns(1);
+        _clusterNodeStore.GetMatchingIndex("node2").Returns(1);
+        _clusterNodeStore.GetMatchingIndex("node3").Returns(2);
+        _replicationStateManager.UpdateCommitIndex([
+            new NodeInfo("node1", new NodeAddress("some", 1)), 
+            new NodeInfo("node2", new NodeAddress("some", 2)),
+            new NodeInfo("node3", new NodeAddress("some", 3))
+        ]);
+        
+        _nodeStateStore.CommitIndex.ShouldBe(1);
+    }
+
+    [Test]
+    public void ShouldNotUpdateState_WhenMatchingIndexTermIsDifferent()
+    {
+        PopulateLog([1, 1]);
+        _nodeStateStore.CurrentTerm = 2;
+        _clusterNodeStore.GetMatchingIndex("node1").Returns(1);
+        _clusterNodeStore.GetMatchingIndex("node2").Returns(1);
+        _clusterNodeStore.GetMatchingIndex("node3").Returns(2);
+        _replicationStateManager.UpdateCommitIndex([
+            new NodeInfo("node1", new NodeAddress("some", 1)), 
+            new NodeInfo("node2", new NodeAddress("some", 2)),
+            new NodeInfo("node3", new NodeAddress("some", 3))
+        ]);
+        
+        _nodeStateStore.CommitIndex.ShouldBe(-1);
+    }
+}
---
+++ Raft.Node/Communication/Services/Cluster/ReplicationStateManager.cs
@@ -0,0 +1,43 @@
+﻿using Raft.Store;
+using Raft.Store.Domain;
+
+namespace Raft.Node.Communication.Services.Cluster;
+
+public class ReplicationStateManager
+{
+    private readonly INodeStateStore _stateStore;
+    private readonly IClusterNodeStore _clusterStore;
+
+    public ReplicationStateManager(INodeStateStore stateStore, IClusterNodeStore clusterStore)
+    {
+        _stateStore = stateStore;
+        _clusterStore = clusterStore;
+    }
+
+    public void UpdateCommitIndex(NodeInfo[] nodes)
+    {
+        if (nodes.Length == 0)
+        {
+            _stateStore.CommitIndex = _stateStore.LogLength - 1;
+            return;
+        }
+        var matchingIndexes = nodes.Select(x => _clusterStore.GetMatchingIndex(x.NodeName)).ToArray();
+        var current = _stateStore.LogLength;
+        while (current > _stateStore.CommitIndex)
+        {
+            var next = current - 1;
+            var termAtNext = _stateStore.GetTermAtIndex(next);
+            if (termAtNext == _stateStore.CurrentTerm && matchingIndexes.Count(x => x >= next) > nodes.Length / 2)
+            {
+                _stateStore.CommitIndex = next;
+                return;
+            }
+            current = next;
+        }
+    }
+
+    private int MatchingCountAtIndex(int[] matchingIndexes, int current)
+    {
+        return matchingIndexes.Count(x => x >= current);
+    }
+}
