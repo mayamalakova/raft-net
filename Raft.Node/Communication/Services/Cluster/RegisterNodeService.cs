70fd4eb3414f47e2de8ab18356bd9f6db09ac183
Maya Malakova <maya.malakova@gmail.com>
2025-03-16 15:04:25 +0100

    propagate new node to known followers when registering

--- Raft.Node/Communication/Services/Cluster/RegisterNodeService.cs
+++ Raft.Node/Communication/Services/Cluster/RegisterNodeService.cs
@@ -1,21 +1,36 @@
 ﻿using Grpc.Core;
 using Raft.Communication.Contract;
+using Raft.Node.Communication.Client;
 using Raft.Store;
 using Raft.Store.Domain;
 using Serilog;
 
 namespace Raft.Node.Communication.Services.Cluster;
 
-public class RegisterNodeService(IClusterNodeStore nodeStore) : RegisterNodeSvc.RegisterNodeSvcBase, INodeService
+public class RegisterNodeService(INodeStateStore stateStore, IClusterNodeStore clusterStore, IClientPool clientPool) : RegisterNodeSvc.RegisterNodeSvcBase, INodeService
 {
     public override Task<RegisterNodeRply> RegisterNode(RegisterNodeRequest request, ServerCallContext context)
     {
-        var result = nodeStore.AddNode(request.Name, new NodeAddress(request.Host, request.Port));
-        Log.Information($"Added node {request.Name}. Cluster members are {nodeStore}");
+        if (stateStore.Role == NodeType.Leader)
+        {
+            UpdateClusterMembersAtFollowers(request);
+        }
+        
+        var result = clusterStore.AddNode(request.Name, new NodeAddress(request.Host, request.Port));
+        Log.Information($"Added node {request.Name}. Cluster members are {clusterStore}");
         return Task.FromResult(new RegisterNodeRply
         {
             Reply = result
         });
+    }
+
+    private void UpdateClusterMembersAtFollowers(RegisterNodeRequest request)
+    {
+        foreach (var node in clusterStore.GetNodes())
+        {
+            var registerNodeClient = clientPool.GetRegisterNodeClient(node.NodeAddress);
+            registerNodeClient.RegisterNode(request);
+        }
     }
 
     public ServerServiceDefinition GetServiceDefinition()
