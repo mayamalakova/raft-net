bf7525e3231d3cd86d3411f6f24c474492efd4df
Maya Malakova <maya.malakova@gmail.com>
2025-03-16 09:19:27 +0100

    extract HaveReconciled

--- Raft.IntegrationTests/RaftLogReplicationTests.cs
+++ Raft.IntegrationTests/RaftLogReplicationTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using Raft.Cli;
 using Raft.Node;
+using Raft.Shared;
 using Raft.Store.Domain;
 using Shouldly;
 
@@ -9,17 +10,25 @@ namespace Raft.IntegrationTests;
 public class RaftLogReplicationTests
 {
     private readonly ICollection<RaftNode> _nodes = new List<RaftNode>();
-    
+
+    [SetUp]
+    public void SetUp()
+    {
+        Logger.ConfigureLogger();
+    }
+
     [TearDown]
     public void TearDown()
     {
+        Logger.Close();
         foreach (var node in _nodes)
         {
             node.Stop();
         }
+
         _nodes.Clear();
     }
-    
+
     [Test]
     public void ShouldReconcileFollowerLogAfterReconnecting()
     {
@@ -30,21 +39,21 @@ public class RaftLogReplicationTests
         var leaderClient = new RaftClient("localhost", 5001);
         var followerClient1 = new RaftClient("localhost", 5002);
         var followerClient2 = new RaftClient("localhost", 5003);
-        
+
         leaderClient.Command(new CommandOptions { Var = "A", Operation = "=", Literal = 1 });
-        followerClient1.LogInfo().ShouldBe( "{ \"entries\": \"(A=1)\" }");
-        followerClient2.LogInfo().ShouldBe( "{ \"entries\": \"(A=1)\" }");
+        followerClient1.LogInfo().ShouldBe("{ \"entries\": \"(A=1)\" }");
+        followerClient2.LogInfo().ShouldBe("{ \"entries\": \"(A=1)\" }");
 
         DisconnectNode(followerClient2, follower2);
         leaderClient.Command(new CommandOptions { Var = "B", Operation = "=", Literal = 1 });
-        followerClient1.LogInfo().ShouldBe( "{ \"entries\": \"(A=1), (B=1)\" }");
-        followerClient2.LogInfo().ShouldBe( "{ \"entries\": \"(A=1)\" }");
+        followerClient1.LogInfo().ShouldBe("{ \"entries\": \"(A=1), (B=1)\" }");
+        followerClient2.LogInfo().ShouldBe("{ \"entries\": \"(A=1)\" }");
 
         ReconnectNode(followerClient2, follower2);
         Task.Delay(4000).Wait();
-        followerClient2.LogInfo().ShouldBe( "{ \"entries\": \"(A=1), (B=1)\" }");
+        followerClient2.LogInfo().ShouldBe("{ \"entries\": \"(A=1), (B=1)\" }");
     }
-    
+
     [Test]
     public void ShouldApplyLatestCommittedAtLeaderAfterReconcilingFollowerToGetMajority()
     {
@@ -56,48 +65,40 @@ public class RaftLogReplicationTests
         var followerClient1 = new RaftClient("localhost", 5002);
         var followerClient2 = new RaftClient("localhost", 5003);
         leader.GetNodeState().ShouldBe("commitIndex=-1, term=0, lastApplied=-1");
-        
+
         // command 1 - A=1
         leaderClient.Command(new CommandOptions { Var = "A", Operation = "=", Literal = 1 });
-        followerClient1.LogInfo().ShouldBe( "{ \"entries\": \"(A=1)\" }");
-        followerClient2.LogInfo().ShouldBe( "{ \"entries\": \"(A=1)\" }");
+        followerClient1.LogInfo().ShouldBe("{ \"entries\": \"(A=1)\" }");
+        followerClient2.LogInfo().ShouldBe("{ \"entries\": \"(A=1)\" }");
         leader.GetNodeState().ShouldBe("commitIndex=0, term=0, lastApplied=0");
 
         // disconnect follower2
         DisconnectNode(followerClient2, follower2);
-        
+
         // command 2 - A=2
         leaderClient.Command(new CommandOptions { Var = "A", Operation = "=", Literal = 2 });
-        followerClient1.LogInfo().ShouldBe( "{ \"entries\": \"(A=1), (A=2)\" }");
-        followerClient2.LogInfo().ShouldBe( "{ \"entries\": \"(A=1)\" }");
+        followerClient1.LogInfo().ShouldBe("{ \"entries\": \"(A=1), (A=2)\" }");
+        followerClient2.LogInfo().ShouldBe("{ \"entries\": \"(A=1)\" }");
         leaderClient.GetState().ShouldBe("value: 1, errors: [ ]");
         leader.GetNodeState().ShouldBe("commitIndex=0, term=0, lastApplied=0");
         follower1.GetNodeState().ShouldBe("commitIndex=0, term=0, lastApplied=0");
-        
+
         // reconnect follower2 and give time to replicate
         ReconnectNode(followerClient2, follower2);
 
-        for (var i = 0; i < 6; i++)
+        for (var i = 0; i < 4; i++)
         {
             Task.Delay(2000).Wait();
-            if (HasReconciled()) break;
+            if (HaveReconciled([leader, follower1, follower2], "commitIndex=1, term=0, lastApplied=1")) break;
             Console.WriteLine($"Checking if they have reconciled... {i + 1}");
         }
-            
+
         leader.GetNodeState().ShouldBe("commitIndex=1, term=0, lastApplied=1");
         follower1.GetNodeState().ShouldBe("commitIndex=1, term=0, lastApplied=1");
         follower2.GetNodeState().ShouldBe("commitIndex=1, term=0, lastApplied=1");
         leaderClient.GetState().ShouldBe("value: 2, errors: [ ]");
         followerClient2.GetState().ShouldBe("value: 2, errors: [ ]");
         followerClient1.GetState().ShouldBe("value: 2, errors: [ ]");
-        return;
-
-        bool HasReconciled()
-        {
-            return leader.GetNodeState() == "commitIndex=1, term=0, lastApplied=1" && 
-                   follower1.GetNodeState() == "commitIndex=1, term=0, lastApplied=1" && 
-                   follower2.GetNodeState() == "commitIndex=1, term=0, lastApplied=1";
-        }
     }
 
     [Test]
@@ -108,12 +109,12 @@ public class RaftLogReplicationTests
 
         const int someLiteral = 5;
         var result = leaderClient.Command(new CommandOptions { Var = "A", Operation = "=", Literal = someLiteral });
-        
+
         result.ShouldContain($"value={someLiteral}");
         leaderClient.GetState().ShouldBe($"value: {someLiteral}, errors: [ ]");
         leader.GetNodeState().ShouldBe("commitIndex=0, term=0, lastApplied=0");
     }
-    
+
     [Test]
     public void ShouldApplyCommittedEntriesOnFollowerAfterReplication()
     {
@@ -123,9 +124,9 @@ public class RaftLogReplicationTests
 
         const int someLiteral = 5;
         leaderClient.Command(new CommandOptions { Var = "A", Operation = "=", Literal = someLiteral });
-        
+
         Task.Delay(4000).Wait();
-        
+
         follower.GetNodeState().ShouldBe("commitIndex=0, term=0, lastApplied=0");
     }
 
@@ -156,12 +157,19 @@ public class RaftLogReplicationTests
         _nodes.Add(leader);
         return leader;
     }
-    
+
     private RaftNode CreateFollower(string name, int port, int peerPort)
     {
         var node = new RaftNode(NodeType.Follower, name, port, "localhost", peerPort, 1, 3);
         node.Start();
         _nodes.Add(node);
         return node;
+    }
+
+    private bool HaveReconciled(RaftNode[] nodes, string desiredState)
+    {
+        var nodeStates = nodes.Select(n => n.GetNodeState()).ToArray();
+        Console.WriteLine(string.Join(", ", nodeStates));
+        return nodeStates.ToHashSet().Count == 1 && nodeStates.First() == desiredState;
     }
 }
