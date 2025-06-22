81e568a617f87d07f27b37755fece8c97e6c397e
Maya Malakova <maya.malakova@gmail.com>
2025-06-22 20:15:26 +0200

    fixup! Provide LeaderPresenceTracker.cs

---
+++ Raft.Node.Tests/MockHelpers/MockTimer.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Timers;
+using ITimer = Raft.Node.Timing.ITimer;
+
+public class MockTimer : ITimer
+{
+    private bool _enabled;
+    private double _interval;
+    private event ElapsedEventHandler? _elapsed;
+
+    public event ElapsedEventHandler Elapsed
+    {
+        add => _elapsed += value;
+        remove => _elapsed -= value;
+    }
+
+    public bool Enabled
+    {
+        get => _enabled;
+        set => _enabled = value;
+    }
+
+    public double Interval
+    {
+        get => _interval;
+        set => _interval = value;
+    }
+
+    public void Start()
+    {
+        _enabled = true;
+    }
+
+    public void Stop()
+    {
+        _enabled = false;
+    }
+
+    // Method to simulate timer expiration
+    public void SimulateElapsed()
+    {
+        if (_enabled && _elapsed != null)
+        {
+            _elapsed.Invoke(this, new ElapsedEventArgs(DateTime.Now));
+        }
+    }
+}
---
+++ Raft.Node.Tests/MockHelpers/MockTimerFactory.cs
@@ -0,0 +1,13 @@
+﻿using NSubstitute;
+using Raft.Node.Timing;
+using ITimer = Raft.Node.Timing.ITimer;
+
+namespace Raft.Node.Tests.MockHelpers;
+
+public class MockTimerFactory(ITimer timer) : ITimerFactory
+{
+    public ITimer CreateTimer()
+    {
+        return timer;
+    }
+}
